a18d4701104b174ece0c8d8372081ba19ae7eb5c
ValentinLutz <allround.email@gmail.com>
2022-01-18 19:42:39 +0100

    Fix docker.down makefile target

--- .make/docker.mk
+++ .make/docker.mk
@@ -5,6 +5,7 @@ docker.up:: ## Start docker containers | PROJECT_NAME
 
 docker.down:: ## Shutdown docker containers | PROJECT_NAME
 	docker-compose -p ${PROJECT_NAME} \
+		-f deployment-docker/docker-compose.yaml \
 		down
 
 docker.app.up:: ## Start app docker container | PROJECT_NAME
