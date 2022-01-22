671817e2809c3d2bd44e6200dcd50993b444fb8a
ValentinLutz <allround.email@gmail.com>
2022-01-22 13:04:44 +0100

    Update database table colums to varchar

--- migration-database/src/main/resources/migration/V2__create_order_item_table.sql
+++ migration-database/src/main/resources/migration/V2__create_order_item_table.sql
@@ -2,8 +2,8 @@ CREATE TABLE spring_native_reference_project.order_item
 (
     id            INT GENERATED ALWAYS AS IDENTITY,
     creation_date TIMESTAMPTZ NOT NULL,
-    order_id      varchar(32) NOT NULL,
-    item_name     text        NOT NULL,
+    order_id      varchar     NOT NULL,
+    item_name     varchar     NOT NULL,
     CONSTRAINT order_id FOREIGN KEY (order_id) REFERENCES "order" (order_id),
     PRIMARY KEY (id)
 );
